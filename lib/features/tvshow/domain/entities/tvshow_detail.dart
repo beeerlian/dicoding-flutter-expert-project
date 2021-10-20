d444fc9046321fe05f65d473332ff3b9ebaf87c4
beeerlian <naufal.berlian.99@gmail.com>
2021-10-20 15:18:36 +0700

    refactor project and create entities

---
+++ lib/features/tvshow/domain/entities/tvshow_detail.dart
@@ -0,0 +1,48 @@
+import 'package:equatable/equatable.dart';
+
+class TvShowDetail extends Equatable {
+  TvShowDetail({
+    required this.id,
+    required this.backdropPath,
+    required this.voteAverage,
+    required this.overview,
+    required this.firstAirDate,
+    required this.originCountry,
+    required this.genreIds,
+    required this.originalLanguage,
+    required this.voteCount,
+    required this.name,
+    required this.originalName,
+  });
+
+  String? posterPath;
+  double? popularity;
+  int id;
+  String? backdropPath;
+  double? voteAverage;
+  String? overview;
+  String? firstAirDate;
+  List<String>? originCountry;
+  List<int>? genreIds;
+  String? originalLanguage;
+  int? voteCount;
+  String? name;
+  String? originalName;
+
+  @override
+  List<Object?> get props => [
+        posterPath,
+        popularity,
+        id,
+        backdropPath,
+        voteAverage,
+        voteCount,
+        overview,
+        firstAirDate,
+        originCountry,
+        originalLanguage,
+        originalName,
+        name,
+        genreIds
+      ];
+}
---
+++ test/features/movie/helpers/test_helper.dart
@@ -0,0 +1,19 @@
+import 'package:ditonton/common/network_info.dart';
+import 'package:ditonton/features/movie/data/datasources/db/database_helper.dart';
+import 'package:ditonton/features/movie/data/datasources/movie_local_data_source.dart';
+import 'package:ditonton/features/movie/data/datasources/movie_remote_data_source.dart';
+
+import 'package:ditonton/features/movie/domain/repositories/movie_repository.dart';
+import 'package:mockito/annotations.dart';
+import 'package:http/http.dart' as http;
+
+@GenerateMocks([
+  MovieRepository,
+  MovieRemoteDataSource,
+  MovieLocalDataSource,
+  DatabaseHelper,
+  NetworkInfo
+], customMocks: [
+  MockSpec<http.Client>(as: #MockHttpClient)
+])
+void main() {}
